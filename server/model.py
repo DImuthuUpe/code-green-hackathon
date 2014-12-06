527e392597946cda58c2d648094b7966ada4a30a
DImuthuUpe <dimuthu.upeksha2@gmail.com>
2014-12-06 16:18:33 +0000

    Created register method

--- server/model.py
+++ server/model.py
@@ -1,3 +1,5 @@
+from server import db
+
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column('id',db.Integer , primary_key=True)
--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, Response
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cors import cross_origin
 from sqlalchemy import text
+import model
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_ECHO'] = False
@@ -11,8 +12,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://@localhost/puppy-earth'
 
 db = SQLAlchemy(app)
 
-   
+
+
 @app.route('/ping/<cookie>')
+@cross_origin()
 def ping(cookie):
     response ={}
     connection = db.engine.connect()
@@ -37,5 +40,17 @@ def index():
     return Response(json.dumps(countries), mimetype='application/json')
     
 
+@app.route('/register',methods=['POST'])
+@cross_origin()
+def register():
+    user_data =  json.loads( request.data )
+    
+    country_id= user_data['country_id']
+    registration = user_data['registration']
+    
+    user = User(1,2,0,'Cookoie')
+    return 'success'
+    
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081,threaded=True)
