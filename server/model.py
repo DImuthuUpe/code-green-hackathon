78595279f6b3273c4a0b8f6a392ef9fd5e3e8ecf
Meni Vaitsi <menivai@gmail.com>
2014-12-06 20:10:41 +0000

    Adding Country model.

--- server/model.py
+++ server/model.py
@@ -1,24 +1,61 @@
+""" Implements the data models. """
+
 from server import db
 import datetime
 
 class User(db.Model):
-    __tablename__ = "user"
-    id = db.Column('id',db.Integer , primary_key=True)
-    country_id = db.Column('country_id', db.Integer)
-    status = db.Column('status' , db.Integer)
-    miles_per_day = db.Column('miles_per_day' , db.Integer)
-    registration = db.Column('registration' , db.String(45))
-    created_date = db.Column('created_date',db.DateTime)
+  """ Implements the User model. """
+  __tablename__ = "user"
+  id = db.Column('id', db.Integer, primary_key=True)
+  country_id = db.Column('country_id', db.Integer)
+  status = db.Column('status', db.Integer)
+  miles_per_day = db.Column('miles_per_day', db.Integer)
+  registration = db.Column('registration', db.String(45))
+  created_date = db.Column('created_date', db.DateTime)
 
-    def __init__(self , country_id ,status, miles_per_day , registration ):
-        self.country_id = country_id
-        self.status = status
-        self.miles_per_day = miles_per_day
-        self.registration = registration
-        self.created_date = datetime.datetime.utcnow()
+  def __init__(self, country_id, status, miles_per_day, registration):
+    self.country_id = country_id
+    self.status = status
+    self.miles_per_day = miles_per_day
+    self.registration = registration
+    self.created_date = datetime.datetime.utcnow()
 
-    def get_id(self):
-        return unicode(self.id)
+  def get_id(self):
+    return unicode(self.id)
 
-    def __repr__(self):
-        return '<User %r>' % (self.id)
+  def __repr__(self):
+    return '<User %r>' % (self.id)
+  
+
+class Country(db.Model):
+  """ Implements the Country model. """
+  __tablename__ = "country"
+  id = db.Column('id', db.Integer, primary_key=True)
+  name = db.Column('name', db.String(65))
+  population = db.Column('population', db.Integer)
+  carbon_per_capita_0 = db.Column('carbon_per_capita_0', db.Float(precision=2))
+  carbon_per_capita_1 = db.Column('carbon_per_capita_1', db.Float(precision=2))
+  carbon_per_capita_2 = db.Column('carbon_per_capita_2', db.Float(precision=2))
+  
+  def __init__(self, name, popultion, carbon_per_capita_0, carbon_per_capita_1, carbon_per_capita_2):
+    self.name = name
+    self.population = population
+    self.carbon_per_capita_0 = carbon_per_capita_0
+    self.carbon_per_capita_1 = carbon_per_capita_1
+    self.carbon_per_capita_2 = carbon_per_capita_2
+
+  def get_id(self):
+    return unicode(self.id)
+
+  def __repr__(self):
+    return '<Country %r>' % (self.id)
+
+
+class Food(db.Model):
+  """ Implements the Food model. """
+  pass
+
+
+class Action(db.Model):
+  """ Implements the Action model. """
+  pass
--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, Response
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cors import cross_origin
 from sqlalchemy import text
+
 from model import *
 
 app = Flask(__name__)
@@ -16,52 +17,49 @@ app.debug = True
 db = SQLAlchemy(app)
 
 
-
 @app.route('/ping/<cookie>')
 @cross_origin()
 def ping(cookie):
-    response ={}
+    response = {}
     connection = db.engine.connect()
-    sql = 'select id from user where registration = "'+cookie+'"'
+    sql = 'select id from user where registration = "' + cookie + '"'
     rows = connection.execute(text(sql))
     for u in rows:
         response = (dict(u))
         
-    return Response(json.dumps(response),mimetype='application/json')
+    return Response(json.dumps(response), mimetype='application/json')
     
 @app.route('/countries')
 @cross_origin()
 def index():
     countries = []
     connection = db.engine.connect()
-    sql = 'select id,name from country'
+    sql = 'select id, name from country'
     rows = connection.execute(text(sql))
     for c in rows:
         countries.append(dict(c.items()))
-        
-        
+
     return Response(json.dumps(countries), mimetype='application/json')
     
-
-@app.route('/register',methods=['POST'])
+@app.route('/register', methods=['POST'])
 @cross_origin()
 def register():
     cookie = str(uuid.uuid4())
-    user_data =  json.loads( request.data )
-    country_id= user_data['country']
+    user_data = json.loads(request.data)
+    country_id = user_data['country']
 
-    response ={}
-    user = User(int(country_id),0,0,cookie)
+    response = {}
+    user = User(int(country_id), 0, 0, cookie)
     try:
         db.session.add(user)
         db.session.flush()
         db.session.commit()
-        response={ 'cookie': cookie }
+        response = { 'cookie': cookie }
     except:
         db.session.rollback()
-        response={'cookie':''}
+        response = {'cookie': ''}
         
     return Response(json.dumps(response), mimetype='application/json')
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081,threaded=True)
+    app.run(host='0.0.0.0', port=8081, threaded=True)
