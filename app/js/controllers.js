58284248549806a20fb6dc65ef895db29492f6e4
koding user <tavernamaurizio@gmail.com>
2014-12-06 14:28:45 +0000

    Added Registration Screen

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,16 +1,55 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCookies'])
 
-.controller('IntroCtrl', function($scope,$location) {
-    $scope.start = function() {
-        $location.path = "#/tab/dash"
+.controller('IntroCtrl', function($scope,$rootScope,$http,$cookies) {
+        $rootScope.in = false;
+         $scope.nextPage = '#/tab/dash';
+         var user = $cookies.puppyEarth;    
+         
+         if( user == undefined) {
+             $scope.nextPage = '#/register';
+         } else {
+            $http.get($rootScope.host+"/ping")
+            .success(function (data) {
+                alert(data)
+            }).error(function () {
+                alert("error")
+            });
+             
+         }
+         
+        
+
+})
+
+.controller('RegisterCtrl', function($scope,$rootScope, $http,$timeout) {
+    $scope.countries = []
+    $timeout(function() {
+    $http.get($rootScope.host+"/")
+        .success(function (data) {
+            $scope.countries = data
+        })
+        
+    },100)
+    $scope.register = function(id) {
+        alert("id:" + id)
+    }
+    
+})
+.controller('DashCtrl', function($scope,$rootScope,$location) {
+    if(!$rootScope.in) {
+        alert("not in")
     }
 })
 
-.controller('DashCtrl', function($scope) {
-})
+.controller('FriendsCtrl', function($scope,$rootScope, $http) {
+  $scope.friends = []
+      $http.get($rootScope.host+"/")
+        .success(function (data) {
+            $scope.friends = data
+        }).error(function () {
+            alert("error")
+        });
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
