5d5a6758db9d69b252f6be79f5a4bbf47c39cd79
DImuthuUpe <dimuthu.upeksha2@gmail.com>
2014-12-06 15:10:59 +0000

    Update user model

---
+++ server/model.py
@@ -0,0 +1,30 @@
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column('id',db.Integer , primary_key=True)
+    country_id = db.Column('country_id', db.Integer)
+    status = db.Column('status' , db.Integer)
+    miles_per_day = db.Column('miles_per_day' , db.Integer)
+    registration = db.Column('registration' , db.String(45))
+    created_date = db.Column('created_date',db.DateTime)
+
+    def __init__(self , country_id ,status, miles_per_day , registration ):
+        self.country_id = country_id
+        self.status = status
+        self.miles_per_day = miles_per_day
+        self.registration = registration
+        self.created_date = datetime.datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
